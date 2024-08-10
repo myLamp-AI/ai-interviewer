98793142e278c0dd84cc38dd80a8b4485e17206f
dynamo_dream <theanish2002kumar@gmail.com>
2024-08-10 16:39:36 +0530

    added websockets

--- interviewer.py
+++ interviewer.py
@@ -10,56 +10,13 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
-from RealtimeTTS import TextToAudioStream, GTTSEngine
+#from RealtimeTTS import TextToAudioStream, GTTSEngine
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-engine = GTTSEngine()  # replace with your TTS engine
-stream = TextToAudioStream(engine)
+# engine = GTTSEngine()  # replace with your TTS engine
+# stream = TextToAudioStream(engine)
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-
-
-def conduct_interview(bot):
-        for stage in bot.stages:
-            print(f"\n--- {stage} STAGE ---")
-            
-            bot.message_history = ChatMessageHistory()
-            if stage == "TECHNICAL":
-                prompt = PROMPTS[stage].format(skills=bot.cv_parts[stage], job_description=bot.job_description)
-            else:
-                prompt = PROMPTS[stage].format(variable=bot.cv_parts[stage])
-
-            while True:
-                response = bot.get_ai_response(prompt, "Ask your question or exit the interview")
-                
-                next_phase = response.find("move to next phase")
-                if next_phase != -1:
-                    response = response[:next_phase]
-                    move_to_new_phase = True
-                else:
-                    move_to_new_phase = False
-                if response:
-                    if "exit" in response or "interview concluded" in response:
-                        break
-
-                print("Interviewer:", response)
-                bot.text_to_speech(response)
-
-                print("Please speak your answer...")
-                answer = bot.speech_to_text()
-                print("You:", answer)
-                if answer:
-                    if "exit" in answer:
-                        return
-
-                bot.message_history.add_user_message(answer)
-
-                if move_to_new_phase:
-                    break
-                bot.result[response] = answer
-        print("\nInterview concluded. Thank you for your time!")
-
-
 
 class InterviewBot:
     def __init__(self, cv_text, job_description,results):
@@ -73,7 +30,7 @@ class InterviewBot:
         self.cv_parts = get_resume_in_parts(self.model, cv_text)
         self.job_description = get_summarized_jd(self.model, job_description)
         # Initialize TTS engine
-        self.stream = stream
+        # self.stream = stream
         
         # Initialize STT recognizer
         
@@ -84,14 +41,14 @@ class InterviewBot:
         text = re.sub(r"[^a-z0-9',!?-]", " ", text)
         return text
 
-    def text_to_speech(self, text):
-        self.stream.feed(text)
-        self.stream.play()
+    # def text_to_speech(self, text):
+    #     self.stream.feed(text)
+    #     self.stream.play()
 
-    def speech_to_text(self):
-        transcriber = AudioTranscriber()
-        transcription = transcriber.run()
-        return transcription
+    # def speech_to_text(self):
+    #     transcriber = AudioTranscriber()
+    #     transcription = transcriber.run()
+    #     return transcription
 
     def get_ai_response(self, prompt, input_text):
         chain = ChatPromptTemplate.from_messages([
@@ -114,45 +71,45 @@ class InterviewBot:
 
         return self.clean_text(response.content)
 
-    def conduct_interview_inside(self):
-        for stage in self.stages:
-            print(f"\n--- {stage} STAGE ---")
+    # def conduct_interview_inside(self):
+    #     for stage in self.stages:
+    #         print(f"\n--- {stage} STAGE ---")
             
-            self.message_history = ChatMessageHistory()
-            if stage == "TECHNICAL":
-                prompt = PROMPTS[stage].format(skills=self.cv_parts[stage], job_description=self.job_description)
-            else:
-                prompt = PROMPTS[stage].format(variable=self.cv_parts[stage])
+    #         self.message_history = ChatMessageHistory()
+    #         if stage == "TECHNICAL":
+    #             prompt = PROMPTS[stage].format(skills=self.cv_parts[stage], job_description=self.job_description)
+    #         else:
+    #             prompt = PROMPTS[stage].format(variable=self.cv_parts[stage])
 
-            while True:
-                response = self.get_ai_response(prompt, "Ask your question or exit the interview")
+    #         while True:
+    #             response = self.get_ai_response(prompt, "Ask your question or exit the interview")
                 
-                next_phase = response.find("move to next phase")
-                if next_phase != -1:
-                    response = response[:next_phase]
-                    move_to_new_phase = True
-                else:
-                    move_to_new_phase = False
-                if response:
-                    if "exit" in response or "interview concluded" in response:
-                        break
+    #             next_phase = response.find("move to next phase")
+    #             if next_phase != -1:
+    #                 response = response[:next_phase]
+    #                 move_to_new_phase = True
+    #             else:
+    #                 move_to_new_phase = False
+    #             if response:
+    #                 if "exit" in response or "interview concluded" in response:
+    #                     break
 
-                print("Interviewer:", response)
-                self.text_to_speech(response)
+    #             print("Interviewer:", response)
+    #             self.text_to_speech(response)
 
-                print("Please speak your answer...")
-                answer = self.speech_to_text()
-                print("You:", answer)
-                if answer:
-                    if "exit" in answer:
-                        return
+    #             print("Please speak your answer...")
+    #             answer = self.speech_to_text()
+    #             print("You:", answer)
+    #             if answer:
+    #                 if "exit" in answer:
+    #                     return
 
-                self.message_history.add_user_message(answer)
+    #             self.message_history.add_user_message(answer)
 
-                if move_to_new_phase:
-                    break
-                self.result[response] = answer
-        print("\nInterview concluded. Thank you for your time!")
+    #             if move_to_new_phase:
+    #                 break
+    #             self.result[response] = answer
+    #     print("\nInterview concluded. Thank you for your time!")
 
 # Usage
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,105 +1,104 @@
-import pyaudio
-import threading
-import speech_recognition as sr
-import time
+# import pyaudio
+# import threading
+# import speech_recognition as sr
+# import time
 import os
 from dotenv import load_dotenv
-from gtts import gTTS
-from io import BytesIO
-from pydub import AudioSegment
-from pydub.playback import play
-import queue
+# from gtts import gTTS
+# from io import BytesIO
+# from pydub import AudioSegment
+# from pydub.playback import play
+# import queue
 from PyPDF2 import PdfReader
-
 load_dotenv()
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# Constants
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-RECORD_SECONDS = 5
-SLEEP_COMMAND = "sleep"
-QUIT_COMMAND = "quit"
+# # Constants
+# CHUNK = 1024
+# FORMAT = pyaudio.paInt16
+# CHANNELS = 1
+# RATE = 16000
+# RECORD_SECONDS = 5
+# SLEEP_COMMAND = "sleep"
+# QUIT_COMMAND = "quit"
 
-API_KEY = os.getenv("GOOGLE_API_KEY")
+# API_KEY = os.getenv("GOOGLE_API_KEY")
 
-def play_audio(text, lang="hi"):
-    mp3_fp = BytesIO()
-    tts = gTTS(text, lang=lang)
-    tts.write_to_fp(mp3_fp)
-    mp3_fp.seek(0)
-    audio = AudioSegment.from_mp3(mp3_fp)
-    play(audio)
+# def play_audio(text, lang="hi"):
+#     mp3_fp = BytesIO()
+#     tts = gTTS(text, lang=lang)
+#     tts.write_to_fp(mp3_fp)
+#     mp3_fp.seek(0)
+#     audio = AudioSegment.from_mp3(mp3_fp)
+#     play(audio)
 
-class AudioTranscriber:
-    def __init__(self):
-        self.recognizer = sr.Recognizer()
-        self.audio_queue = queue.Queue()
-        self.is_recording = True
-        self.transcription = ""
-        self.is_exiting = False
-        self.silence_count = 0
-        self.max_silence_count = 1
+# class AudioTranscriber:
+#     def __init__(self):
+#         self.recognizer = sr.Recognizer()
+#         self.audio_queue = queue.Queue()
+#         self.is_recording = True
+#         self.transcription = ""
+#         self.is_exiting = False
+#         self.silence_count = 0
+#         self.max_silence_count = 1
 
-    def transcribe_audio(self):
-        while True:
-            audio = self.audio_queue.get()
-            if audio is None:
-                break
-            try:
-                text = self.recognizer.recognize_google(audio)
-                self.transcription += text + " "
-                print(text, end=" ")
-                self.silence_count = 0
-                if SLEEP_COMMAND in text.lower():
-                    self.is_recording = False
-                elif QUIT_COMMAND in text.lower():
-                    self.is_recording = False
-                    self.is_exiting = True
-                    print("Thank you for your response.")
-                    return
-            except sr.UnknownValueError:
-                self.silence_count += 1
-                if self.silence_count >= self.max_silence_count:
-                    self.is_recording = False
-            except sr.RequestError as e:
-                print(f"Could not request results from Google Speech Recognition service; {e}")
+#     def transcribe_audio(self):
+#         while True:
+#             audio = self.audio_queue.get()
+#             if audio is None:
+#                 break
+#             try:
+#                 text = self.recognizer.recognize_google(audio)
+#                 self.transcription += text + " "
+#                 print(text, end=" ")
+#                 self.silence_count = 0
+#                 if SLEEP_COMMAND in text.lower():
+#                     self.is_recording = False
+#                 elif QUIT_COMMAND in text.lower():
+#                     self.is_recording = False
+#                     self.is_exiting = True
+#                     print("Thank you for your response.")
+#                     return
+#             except sr.UnknownValueError:
+#                 self.silence_count += 1
+#                 if self.silence_count >= self.max_silence_count:
+#                     self.is_recording = False
+#             except sr.RequestError as e:
+#                 print(f"Could not request results from Google Speech Recognition service; {e}")
 
-    def record_audio(self):
-        p = pyaudio.PyAudio()
-        stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+#     def record_audio(self):
+#         p = pyaudio.PyAudio()
+#         stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
-        print("Recording Started...")
-        while self.is_recording:
-            frames = []
-            for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-                data = stream.read(CHUNK)
-                frames.append(data)
+#         print("Recording Started...")
+#         while self.is_recording:
+#             frames = []
+#             for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+#                 data = stream.read(CHUNK)
+#                 frames.append(data)
 
-            audio_data = sr.AudioData(b''.join(frames), RATE, 2)
-            self.audio_queue.put(audio_data)
+#             audio_data = sr.AudioData(b''.join(frames), RATE, 2)
+#             self.audio_queue.put(audio_data)
 
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-        self.audio_queue.put(None)
+#         stream.stop_stream()
+#         stream.close()
+#         p.terminate()
+#         self.audio_queue.put(None)
 
-    def run(self):
-        transcription_thread = threading.Thread(target=self.transcribe_audio)
-        recording_thread = threading.Thread(target=self.record_audio)
+#     def run(self):
+#         transcription_thread = threading.Thread(target=self.transcribe_audio)
+#         recording_thread = threading.Thread(target=self.record_audio)
         
-        transcription_thread.start()
-        recording_thread.start()
+#         transcription_thread.start()
+#         recording_thread.start()
 
-        while self.is_recording:
-            time.sleep(0.1)
+#         while self.is_recording:
+#             time.sleep(0.1)
 
-        recording_thread.join()
-        transcription_thread.join()
-        print("\nRecording Stopped...")
-        return self.transcription if self.transcription else None
+#         recording_thread.join()
+#         transcription_thread.join()
+#         print("\nRecording Stopped...")
+#         return self.transcription if self.transcription else None
 
 def read_cv(cv_path):
     with open(cv_path, 'rb') as file:
